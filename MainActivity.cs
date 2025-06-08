193dd14d6ca7ae5069b5e3ace9d5a2d6849c06d2
Yalort <banjo_10441@hotmail.com>
2025-06-07 20:52:36 -0400

    Add recipes and ingredients tabs

--- MainActivity.cs
+++ MainActivity.cs
@@ -11,16 +11,32 @@ namespace Potionapp_Mobile
             { "Animal Brew", new Dictionary<string, int>{{"animal",1},{"magic",1},{"solution",1}} }
         };
 
+        readonly Dictionary<string, List<string>> potionSpecialRequirements = new();
+        readonly List<string> specialIngredientsStock = new();
+
         Dictionary<string, EditText>? ingredientFields;
         List<string>? selectedPotions;
         ArrayAdapter<string>? listAdapter;
+        ArrayAdapter<string>? recipesAdapter;
+        ArrayAdapter<string>? specialAdapter;
+        List<string>? recipeNames;
 
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.main_tabs);
+
+            var tabHost = FindViewById<TabHost>(Android.Resource.Id.TabHost)!;
+            tabHost.Setup();
+
+            tabHost.AddTab(tabHost.NewTabSpec("potions").SetIndicator("Potions").SetContent(Resource.Id.tab_potions));
+            tabHost.AddTab(tabHost.NewTabSpec("recipes").SetIndicator("Recipes").SetContent(Resource.Id.tab_recipes));
+            tabHost.AddTab(tabHost.NewTabSpec("ingredients").SetIndicator("Ingredients").SetContent(Resource.Id.tab_ingredients));
+
+            LayoutInflater.Inflate(Resource.Layout.activity_main, tabHost.TabContentView.FindViewById(Resource.Id.tab_potions), true);
+            LayoutInflater.Inflate(Resource.Layout.recipes_tab, tabHost.TabContentView.FindViewById(Resource.Id.tab_recipes), true);
+            LayoutInflater.Inflate(Resource.Layout.ingredients_tab, tabHost.TabContentView.FindViewById(Resource.Id.tab_ingredients), true);
 
             ingredientFields = new Dictionary<string, EditText>
             {
@@ -74,10 +90,94 @@ namespace Potionapp_Mobile
                             }
                         }
                     }
+
+                    if (potionSpecialRequirements.TryGetValue(potion, out var specials))
+                    {
+                        foreach (var sp in specials)
+                            specialIngredientsStock.Remove(sp);
+                        specialAdapter?.NotifyDataSetChanged();
+                    }
                 }
 
                 selectedPotions.Clear();
                 listAdapter!.NotifyDataSetChanged();
+            };
+
+            // Recipes tab setup
+            recipeNames = potionRequirements.Keys.ToList();
+            var recipesList = FindViewById<ListView>(Resource.Id.recipes_list)!;
+            recipesAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, recipeNames);
+            recipesList.Adapter = recipesAdapter;
+            recipesList.ChoiceMode = ChoiceMode.Single;
+
+            var addRecipeBtn = FindViewById<Button>(Resource.Id.add_recipe_button)!;
+            var removeRecipeBtn = FindViewById<Button>(Resource.Id.remove_recipe_button)!;
+
+            addRecipeBtn.Click += (s, e) =>
+            {
+                var name = FindViewById<EditText>(Resource.Id.recipe_name)!.Text;
+                if (string.IsNullOrWhiteSpace(name) || potionRequirements.ContainsKey(name))
+                    return;
+
+                var reqs = new Dictionary<string, int>();
+                foreach (var key in ingredientFields!.Keys)
+                {
+                    var id = Resources.GetIdentifier($"recipe_{key}", "id", PackageName);
+                    var field = FindViewById<EditText>(id)!;
+                    if (int.TryParse(field.Text, out int val) && val > 0)
+                        reqs[key] = val;
+                }
+
+                potionRequirements[name] = reqs;
+                recipeNames!.Add(name);
+
+                var specialsText = FindViewById<EditText>(Resource.Id.recipe_special)!.Text;
+                if (!string.IsNullOrWhiteSpace(specialsText))
+                    potionSpecialRequirements[name] = specialsText.Split(',').Select(s => s.Trim()).Where(s => s.Length > 0).ToList();
+
+                recipesAdapter!.NotifyDataSetChanged();
+                potionSpinner.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, potionRequirements.Keys.ToList());
+            };
+
+            removeRecipeBtn.Click += (s, e) =>
+            {
+                if (recipesList.CheckedItemPosition >= 0 && recipesList.CheckedItemPosition < recipeNames!.Count)
+                {
+                    var name = recipeNames[recipesList.CheckedItemPosition];
+                    recipeNames.RemoveAt(recipesList.CheckedItemPosition);
+                    potionRequirements.Remove(name);
+                    potionSpecialRequirements.Remove(name);
+                    recipesAdapter!.NotifyDataSetChanged();
+                    potionSpinner.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, potionRequirements.Keys.ToList());
+                }
+            };
+
+            // Special ingredients tab setup
+            var specialList = FindViewById<ListView>(Resource.Id.special_list)!;
+            specialAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, specialIngredientsStock);
+            specialList.Adapter = specialAdapter;
+            specialList.ChoiceMode = ChoiceMode.Single;
+
+            var addSpecialBtn = FindViewById<Button>(Resource.Id.add_special_button)!;
+            var removeSpecialBtn = FindViewById<Button>(Resource.Id.remove_special_button)!;
+
+            addSpecialBtn.Click += (s, e) =>
+            {
+                var name = FindViewById<EditText>(Resource.Id.special_name)!.Text;
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    specialIngredientsStock.Add(name.Trim());
+                    specialAdapter!.NotifyDataSetChanged();
+                }
+            };
+
+            removeSpecialBtn.Click += (s, e) =>
+            {
+                if (specialList.CheckedItemPosition >= 0 && specialList.CheckedItemPosition < specialIngredientsStock.Count)
+                {
+                    specialIngredientsStock.RemoveAt(specialList.CheckedItemPosition);
+                    specialAdapter!.NotifyDataSetChanged();
+                }
             };
         }
     }
